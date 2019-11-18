86b809685e7310e73f0ec4831112226262e58495
Aaron Finsrud <52397472+mrfinster100@users.noreply.github.com>
2019-11-18 13:51:09 +0800

    Update directory_search.py

--- directory_search.py
+++ directory_search.py
@@ -3,7 +3,7 @@
 
 ## Setup:
 # 1) Run file.reg to enable drag-and-drop conversion
-# 2) Install necessary Python 3.8 dependencies: (i.e. zipfile, PyPDF2, docx, xlrd)
+# 2) Install necessary Python 3.8 dependencies: (i.e. PyPDF2, docx, xlrd, python-pptx)
 # 3) Drag and drop any parent directory or file on top of directory_search.py
 # 4) Specify a searchable string (not case-sensitive)
 # 5) View list of files containing the specified string w/ instances
